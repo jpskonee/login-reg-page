8fa6f5baf3e700803ad82de92759bf112e067ccb
AGHO-DEV <aghoemmanuel.steam@gmail.com>
2021-05-18 05:20:08 +0100

    first commit

--- src/components/Home/LoginReg.js
+++ src/components/Home/LoginReg.js
@@ -1,11 +1,23 @@
+import { Container, Grid } from '@material-ui/core'
 import React from 'react'
-import BrandName from "../BrandName"
-
+import BrandName from "../BrandName";
+import TextField from '@material-ui/core/TextField';
 
 const LoginReg = () => {
     return (
-        <section className="login-page">
-            <BrandName />
+        <section>
+            <Container className="login-page" maxWidth="sm">
+                <BrandName />
+                <form>
+                    <div>
+                        <input placeholder="Email" />
+                    </div>
+                    <div>
+                        <input placeholder="Password" />
+                    </div>
+                    <div className="login-btn"> Login </div>
+                </form>
+            </Container>
         </section>
     )
 }
